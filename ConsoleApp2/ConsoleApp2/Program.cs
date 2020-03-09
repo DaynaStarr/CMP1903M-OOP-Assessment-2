ebfaab67134e7c6792f1b75816a70452ad69de17
DaynaStarr <60393909+DaynaStarr@users.noreply.github.com>
2020-03-09 11:12:27 +0000

    Correct copy of "difference" code
    working copy

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CompareFiles
+{
+    public class Compare
+    {
+        static void Main(string[] args)
+        {
+
+            //Read file1
+            StreamReader reader;
+            reader = new System.IO.StreamReader("a.txt");
+            string Data1 = reader.ReadToEnd();
+            reader.Close();
+
+            //Read file2
+
+            reader = new System.IO.StreamReader("b.txt");
+            string Data2 = reader.ReadToEnd();
+            reader.Close();
+            if (Data1 == Data2)
+            {
+                Console.WriteLine("Files are the same");
+            }
+            else
+            {
+                Console.WriteLine("Files are different");
+            }
+        }
+    }
+}
